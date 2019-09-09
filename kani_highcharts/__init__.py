af87eeee27a8f56e8d8287364b513d6c1e6396d1
fx_kirin <fx.kirin@gmail.com>
2019-09-09 22:24:09 +0900

    fix

--- kani_highcharts/__init__.py
+++ kani_highcharts/__init__.py
@@ -5,3 +5,5 @@ from .version import version_info, __version__
 from .highcharts.highcharts import Highchart
 from .highmaps.highmaps import Highmap
 from .highstock.highstock import Highstock
+
+from . import ipynb
